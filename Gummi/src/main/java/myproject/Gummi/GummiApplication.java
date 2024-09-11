1a8e28a3fb22c5074210d5c6cbe2e9ab482efaee
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-09-11 15:22:02 +0900

    env: initial setting

---
+++ Gummi/src/main/java/myproject/Gummi/GummiApplication.java
@@ -0,0 +1,13 @@
+package myproject.Gummi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GummiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GummiApplication.class, args);
+	}
+
+}
---
+++ Gummi/src/test/java/myproject/Gummi/GummiApplicationTests.java
@@ -0,0 +1,13 @@
+package myproject.Gummi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GummiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
