b0215a2d1ba6f5a93f1ae669c7952fc9cd951b32
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-09-19 17:22:07 +0900

    feat: weight 객체 생성

--- Gummi/src/main/java/myproject/Gummi/domain/entity/Weight.java
+++ Gummi/src/main/java/myproject/Gummi/domain/entity/Weight.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Entity
 @Setter
 @Getter
@@ -15,6 +17,12 @@ public class Weight {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private double weight;
+    private LocalDate calDate;
+    private boolean deletedAt;
 
+    public Weight() {
+    }
 
+    
 }
